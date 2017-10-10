82f3255eb06d81f6ebf44c2c231b1b80112de2e0
samuelboediono <samuel_boediono@hotmail.com>
2017-10-09 20:13:25 -0700

    updatedfiles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -143,7 +143,67 @@ $(document).ready(function() {
     runRestaurantQuery(numResults, restaurantQueryURL);
 
     $("#restaurant-lists").show();
+    $("#direction-row").show();
   });
 
 });
 
+
+
+
+
+
+
+
+
+var access_token = 'pk.eyJ1IjoibWFodGFiMTIiLCJhIjoiY2o4ZXppdDRrMTh0dTMzbXNjY3NoMnN0OCJ9.6jXHANiUibQbeXNIljgFUQ';
+    var userAddress = '';
+    var userLat = '';
+    var userLon = '';
+    var restAddress = '';
+    var restLat = '';
+    var restLon = '';
+    var steps = true;
+    var stepCount = 1;
+    var queryURLBase = 'https://api.mapbox.com/directions/v5/mapbox/walking/';
+    function runQuery(queryURL) {
+      $.ajax({
+        url: queryURL,
+        method: 'GET'
+      }).done(function(directionData) {  
+          // console.log(directionData);
+        stepsLength = directionData.routes[0].legs[0].steps.length;
+          //console.log('stepsLength: ' + stepsLength);
+        for (i = 0; i < stepsLength; i++) {
+          userDirection = directionData.routes[0].legs[0].steps[i].maneuver.instruction;
+          
+          $('#display-direction').append('<p><span class="label label-primary">' +
+            stepCount + '</span> ' + userDirection + '</p>');
+          stepCount++;
+        }
+        $('#display-direction').append('<p><strong>Enjoy your lunch date!</strong></p>');
+          
+        $('#display-directions').show();
+      });
+    };
+    $('#submit-btn').on('click', function(event) {
+      event.preventDefault();
+      $('#display-direction').empty();
+      userLat = $('#user-lat').val().trim();
+        console.log(userLat);
+      userLon = $('#user-lon').val().trim();
+        console.log(userLon);
+      restLat = $('#rest-lat').val().trim();
+        console.log(restLat);
+      restLon = $('#rest-lon').val().trim();
+        console.log(restLon);
+      //userAddress = encodeURIComponent(userAddress);
+      
+      // https://api.mapbox.com/directions/v5/mapbox/cycling/-122.42,37.78;-77.03,38.91?steps=true&alternatives=true&access_token=your-access-token
+      queryURL = queryURLBase + userLon + ',' + userLat + 
+        ';' + restLon + ',' + restLat + 
+        '?access_token=' + access_token + '&steps=' + steps;
+        // console.log(queryURL);
+      // $('#display-directions').append('Click to see directions JSON: <a href="' + queryURL + '" target="_blank">https://api.mapbox.com/directions/v5/mapbox/walking/...</a>');
+      runQuery(queryURL);
+    });
